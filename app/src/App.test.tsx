55007305dd5bfdc8a20d8b012e5d2a41b794e332
Jacob Rothstein <hi@jbr.me>
2023-03-29 11:55:23 -0700

    fix test

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,5 +1,8 @@
-//import React from "react";
-//import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Router from "./router";
 test("tests have not been written yet but we need at least one test", () => {
-  expect(true).toBeTruthy();
+  render(<Router />);
+  expect(screen.queryByText("test")).toBeNull();
 });
