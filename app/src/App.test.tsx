79427f315464680e262a1756fac18406de869558
Jacob Rothstein <hi@jbr.me>
2023-03-29 11:34:48 -0700

    add a placeholder test for ci

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,10 +1,5 @@
-/* import React from 'react';
- * import { render, screen } from '@testing-library/react';
- * import App from './App';
- *
- * test('renders learn react link', () => {
- *   render(<App />);
- *   const linkElement = screen.getByText(/learn react/i);
- *   expect(linkElement).toBeInTheDocument();
- * }); */
-export {};
+//import React from "react";
+//import { render, screen } from "@testing-library/react";
+test("tests have not been written yet but we need at least one test", () => {
+  expect(true).toBeTruthy();
+});
