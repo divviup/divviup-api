431a32df5cf9036172131ee89fb04dec69923fda
Jacob Rothstein <hi@jbr.me>
2023-03-31 12:33:12 -0700

    add test stubs for remaining tests

---
+++ tests/memberships.rs
@@ -0,0 +1,61 @@
+mod delete {
+    #[test]
+    #[ignore]
+    fn as_member() {}
+
+    #[test]
+    #[ignore]
+    fn not_member() {}
+
+    #[test]
+    #[ignore]
+    fn nonexistant_id() {}
+
+    #[test]
+    #[ignore]
+    fn removing_self() {}
+
+    #[test]
+    #[ignore]
+    fn admin_not_member() {}
+}
+
+mod create {
+    #[test]
+    #[ignore]
+    fn success() {}
+
+    #[test]
+    #[ignore]
+    fn invalid() {}
+
+    #[test]
+    #[ignore]
+    fn not_member() {}
+
+    #[test]
+    #[ignore]
+    fn nonexistant_account() {}
+
+    #[test]
+    #[ignore]
+    fn admin_not_member() {}
+}
+
+mod index {
+    #[test]
+    #[ignore]
+    fn member() {}
+
+    #[test]
+    #[ignore]
+    fn not_member() {}
+
+    #[test]
+    #[ignore]
+    fn nonexistant_account() {}
+
+    #[test]
+    #[ignore]
+    fn admin_not_member() {}
+}
