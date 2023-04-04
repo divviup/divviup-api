296de787f1d94505857aa0e1839a2e38800793f4
David Cook <dcook@divviup.org>
2023-04-04 11:45:57 -0500

    Add build info metric

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,11 +1,14 @@
 use std::sync::Arc;
 
+use git_version::git_version;
 use opentelemetry::{
+    global,
     metrics::MetricsError,
     sdk::{
         export::metrics::aggregation::stateless_temporality_selector,
         metrics::{controllers, processors, selectors::simple::histogram},
     },
+    Context, KeyValue,
 };
 use opentelemetry_prometheus::{Encoder, TextEncoder};
 use tokio::spawn;
@@ -26,6 +29,28 @@ pub fn install_metrics_exporter(host: &str, port: u16) -> Result<(), MetricsErro
             .build(),
         )
         .try_init()?,
+    );
+
+    // Record the binary's version information in a build info metric.
+    let meter = global::meter("divviup-api");
+    let gauge = meter
+        .u64_observable_gauge("divviup_api_build_info")
+        .with_description("Build-time version information")
+        .init();
+    let mut git_revision: &str = git_version!(fallback = "unknwon");
+    if git_revision == "unknown" {
+        if let Some(value) = option_env!("GIT_REVISION") {
+            git_revision = value;
+        }
+    }
+    gauge.observe(
+        &Context::current(),
+        1,
+        &[
+            KeyValue::new("version", env!("CARGO_PKG_VERSION")),
+            KeyValue::new("revision", git_revision),
+            KeyValue::new("rust_version", env!("RUSTC_SEMVER")),
+        ],
     );
 
     let router = Router::new().get("metrics", move |conn: trillium::Conn| {
