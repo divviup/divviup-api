82a009c5b9cc21a7207b461eabe1ce7cec20d23e
Jacob Rothstein <hi@jbr.me>
2023-04-10 15:33:03 -0700

    allow an divviup-api deploy to be healthy without an aggregator-api

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,12 +1,8 @@
-use crate::{handler::Error, AggregatorClient, Db};
+use crate::{handler::Error, Db};
 use sea_orm::ConnectionTrait;
 use trillium::{Conn, Status};
 
-pub async fn health_check(
-    _: &mut Conn,
-    (db, api_client): (Db, AggregatorClient),
-) -> Result<Status, Error> {
+pub async fn health_check(_: &mut Conn, db: Db) -> Result<Status, Error> {
     db.execute_unprepared("select 1").await?;
-    api_client.health_check().await?;
     Ok(Status::Ok)
 }
