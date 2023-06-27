90da966984c5aea72bcc97d0610ee51e2cd1ab16
Jacob Rothstein <hi@jbr.me>
2023-06-27 15:12:05 -0700

    only populate admin if None (#240)

--- tests/users.rs
+++ tests/users.rs
@@ -13,10 +13,11 @@ mod get_users_me {
             .run_async(&app)
             .await;
 
-        let response_user: User = conn.response_json().await;
-
-        assert_eq!(user, response_user);
+        let mut response_user: User = conn.response_json().await;
         assert!(!response_user.is_admin());
+
+        response_user.admin = None; // for equality comparison
+        assert_eq!(user, response_user);
         Ok(())
     }
 
@@ -25,13 +26,31 @@ mod get_users_me {
         let (admin, ..) = fixtures::admin(&app).await;
         let mut conn = get("/api/users/me")
             .with_api_headers()
-            .with_state(admin)
+            .with_state(admin.clone())
+            .run_async(&app)
+            .await;
+
+        let mut response_user: User = conn.response_json().await;
+        assert!(response_user.is_admin());
+
+        response_user.admin = None; // for equality comparison
+        assert_eq!(admin, response_user);
+        Ok(())
+    }
+
+    #[test(harness = set_up)]
+    async fn as_integration_testing_user(app: DivviupApi) -> TestResult {
+        let user = User::for_integration_testing();
+        let mut conn = get("/api/users/me")
+            .with_api_headers()
+            .with_state(user.clone())
             .run_async(&app)
             .await;
 
         let response_user: User = conn.response_json().await;
-
         assert!(response_user.is_admin());
+
+        assert_eq!(user, response_user);
         Ok(())
     }
 }
