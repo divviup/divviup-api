431e0e0731cf2d620c3de2574cf56aedaa5b5736
Jacob Rothstein <hi@jbr.me>
2023-07-25 16:45:57 -0700

    cli: fix aggregator create

--- cli/src/aggregators.rs
+++ cli/src/aggregators.rs
@@ -2,44 +2,15 @@ use crate::{CliResult, Output};
 use clap::Subcommand;
 use divviup_client::{DivviupClient, NewAggregator, Url, Uuid};
 
-#[derive(clap::ValueEnum, Clone, Debug)]
-pub enum Role {
-    Either,
-    Leader,
-    Helper,
-}
-
-impl From<Role> for divviup_client::Role {
-    fn from(value: Role) -> Self {
-        match value {
-            Role::Either => Self::Either,
-            Role::Leader => Self::Leader,
-            Role::Helper => Self::Helper,
-        }
-    }
-}
-
 #[derive(Subcommand, Debug)]
 pub enum AggregatorAction {
     List,
     Create {
-        /// Possible roles for this aggregator
-        ///
-        /// Acceptable values: "Either", "Leader", or "Helper"
-        #[arg(short, long)]
-        role: Role,
-
         /// Human-readable identifier for this aggregator
         ///
         /// This can be changed later
         #[arg(short, long)]
         name: String,
-
-        /// DAP URL for this aggregator
-        ///
-        /// must be https
-        #[arg(short, long)]
-        dap_url: Url,
 
         /// API URL for this aggregator
         ///
@@ -71,9 +42,7 @@ impl AggregatorAction {
             AggregatorAction::List => output.display(client.aggregators(account_id).await?),
 
             AggregatorAction::Create {
-                role,
                 name,
-                dap_url,
                 api_url,
                 bearer_token,
             } => output.display(
@@ -81,9 +50,7 @@ impl AggregatorAction {
                     .create_aggregator(
                         account_id,
                         NewAggregator {
-                            role: role.into(),
                             name,
-                            dap_url,
                             api_url,
                             bearer_token,
                         },
