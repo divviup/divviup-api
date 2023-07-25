3a4fc4a19225f199b5b3bc407d32e2bc4f1c59fe
Jacob Rothstein <hi@jbr.me>
2023-07-25 12:26:12 -0700

    interface to delete shared aggregator (#338)

--- app/src/shared-aggregators/SharedAggregatorList.tsx
+++ app/src/shared-aggregators/SharedAggregatorList.tsx
@@ -1,15 +1,24 @@
 import {
+  Button,
   Col,
   ListGroup,
   ListGroupItem,
+  Modal,
   Placeholder,
   Row,
 } from "react-bootstrap";
-import { Await, useLoaderData } from "react-router-dom";
+import {
+  Await,
+  useFetcher,
+  useLoaderData,
+  useNavigation,
+} from "react-router-dom";
 import { Aggregator } from "../ApiClient";
 import "@github/relative-time-element";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import SharedAggregatorForm from "./SharedAggregatorForm";
+import { Trash } from "react-bootstrap-icons";
+import React from "react";
 
 export const Component = JobQueue;
 
@@ -62,6 +71,58 @@ function AggregatorRow({ aggregator }: { aggregator: Aggregator }) {
           )}
         </code>
       </pre>
+      <DeleteAggregatorButton aggregator={aggregator} />
     </ListGroupItem>
+  );
+}
+
+function DeleteAggregatorButton({ aggregator }: { aggregator: Aggregator }) {
+  const navigation = useNavigation();
+
+  const [show, setShow] = useState(false);
+  const close = React.useCallback(() => setShow(false), []);
+  const open = React.useCallback(() => setShow(true), []);
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    if (fetcher.data) close();
+  }, [fetcher, close]);
+
+  return (
+    <>
+      <Button
+        variant="outline-danger"
+        className="ml-auto"
+        size="sm"
+        onClick={open}
+      >
+        <Trash />
+      </Button>
+      <Modal show={show} onHide={close}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            Confirm Aggregator Deletion ({aggregator.name})
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          This aggregator will immediately be removed from the interface and no
+          new tasks can be created with it.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={close}>
+            Close
+          </Button>
+          <fetcher.Form method="delete" action={aggregator.id}>
+            <Button
+              variant="danger"
+              type="submit"
+              disabled={navigation.state === "submitting"}
+            >
+              <Trash /> Delete
+            </Button>
+          </fetcher.Form>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
--- app/src/shared-aggregators/index.tsx
+++ app/src/shared-aggregators/index.tsx
@@ -10,5 +10,19 @@ export default function sharedAggregators(apiClient: ApiClient): RouteObject {
     async loader() {
       return defer({ aggregators: apiClient.sharedAggregators() });
     },
+    children: [
+      {
+        path: ":aggregator_id",
+        async action({ request, params }) {
+          switch (request.method) {
+            case "DELETE":
+              await apiClient.deleteAggregator(params.aggregator_id as string);
+              return null;
+            default:
+              throw new Error(`unexpected method {request.method}`);
+          }
+        },
+      },
+    ],
   };
 }
