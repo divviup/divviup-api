ce7479bfc1b7d2daf9e6a951bf1825289d2fc452
Jacob Rothstein <hi@jbr.me>
2023-07-25 17:52:58 -0700

    halt at the end of api mocks

--- src/api_mocks/auth0.rs
+++ src/api_mocks/auth0.rs
@@ -2,29 +2,32 @@ use super::random_chars;
 use crate::{clients::auth0_client::Token, User};
 use serde_json::json;
 use trillium::Handler;
-use trillium_api::Json;
+use trillium_api::{Halt, Json};
 use trillium_router::router;
 
 pub fn mock(auth0_url: &str) -> impl Handler {
-    router()
-        .get("/userinfo", Json(User::for_integration_testing()))
-        .post(
-            "/oauth/token",
-            Json(Token {
-                access_token: "access token".into(),
-                expires_in: 60,
-                scope: "".into(),
-                token_type: "bearer".into(),
-            }),
-        )
-        .post(
-            "/api/v2/users",
-            Json(json!({ "user_id": random_chars(10) })),
-        )
-        .post(
-            "/api/v2/tickets/password-change",
-            Json(json!({
-                "ticket": format!("{auth0_url}/password_tickets/{}", random_chars(10))
-            })),
-        )
+    (
+        router()
+            .get("/userinfo", Json(User::for_integration_testing()))
+            .post(
+                "/oauth/token",
+                Json(Token {
+                    access_token: "access token".into(),
+                    expires_in: 60,
+                    scope: "".into(),
+                    token_type: "bearer".into(),
+                }),
+            )
+            .post(
+                "/api/v2/users",
+                Json(json!({ "user_id": random_chars(10) })),
+            )
+            .post(
+                "/api/v2/tickets/password-change",
+                Json(json!({
+                    "ticket": format!("{auth0_url}/password_tickets/{}", random_chars(10))
+                })),
+            ),
+        Halt,
+    )
 }
--- src/api_mocks/postmark.rs
+++ src/api_mocks/postmark.rs
@@ -1,7 +1,7 @@
 use serde_json::json;
 use trillium::Handler;
-use trillium_api::Json;
+use trillium_api::{Halt, Json};
 use trillium_router::router;
 pub fn mock() -> impl Handler {
-    router().post("/email/withTemplate", Json(json!({})))
+    (router().post("/email/withTemplate", Json(json!({}))), Halt)
 }
