64a2e79046ae96c697357267e523f217794a7514
David Cook <dcook@divviup.org>
2023-10-06 16:01:46 -0500

    Update to opentelemetry 0.20

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,54 +2,45 @@ use git_version::git_version;
 use opentelemetry::{
     global,
     metrics::MetricsError,
-    sdk::{
-        export::metrics::aggregation::stateless_temporality_selector,
-        metrics::{controllers, processors, selectors::simple::histogram},
-    },
-    Context, KeyValue,
+    sdk::{metrics::MeterProvider, Resource},
+    KeyValue,
 };
-use std::sync::Arc;
+use prometheus::Registry;
 
 /// Install a Prometheus metrics provider and exporter. The
 /// OpenTelemetry global API can be used to create and update
 /// instruments, and they will be sent through this exporter.
 pub fn metrics_exporter() -> Result<impl trillium::Handler, MetricsError> {
-    let exporter = Arc::new(
-        opentelemetry_prometheus::exporter(
-            controllers::basic(processors::factory(
-                histogram([
-                    0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
-                ]),
-                stateless_temporality_selector(),
-            ))
-            .build(),
-        )
-        .try_init()?,
-    );
+    let registry = Registry::new();
+    let exporter = opentelemetry_prometheus::exporter()
+        .with_registry(registry.clone())
+        .build()?;
 
-    // Record the binary's version information in a build info metric.
-    let meter = global::meter("divviup-api");
-    let gauge = meter
-        .u64_observable_gauge("divviup_api_build_info")
-        .with_description("Build-time version information")
-        .init();
+    // Note that the implementation of `Default` pulls in attributes set via environment variables.
+    let default_resource = Resource::default();
+
     let mut git_revision: &str = git_version!(fallback = "unknown");
     if git_revision == "unknown" {
         if let Some(value) = option_env!("GIT_REVISION") {
             git_revision = value;
         }
     }
-    gauge.observe(
-        &Context::current(),
-        1,
-        &[
-            KeyValue::new("version", env!("CARGO_PKG_VERSION")),
-            KeyValue::new("revision", git_revision),
-            KeyValue::new("rust_version", env!("RUSTC_SEMVER")),
-        ],
-    );
+    let version_info_resource = Resource::new([
+        KeyValue::new(
+            "service.version",
+            format!("{}-{}", env!("CARGO_PKG_VERSION"), git_revision),
+        ),
+        KeyValue::new("process.runtime.name", "Rust"),
+        KeyValue::new("process.runtime.version", env!("RUSTC_SEMVER")),
+    ]);
 
-    Ok(trillium_prometheus::text_format_handler(
-        exporter.registry().clone(),
-    ))
+    let resource = version_info_resource.merge(&default_resource);
+
+    let meter_provider = MeterProvider::builder()
+        .with_reader(exporter)
+        .with_resource(resource)
+        .build();
+    global::set_meter_provider(meter_provider);
+
+    Ok(trillium_prometheus::text_format_handler(registry))
 }
