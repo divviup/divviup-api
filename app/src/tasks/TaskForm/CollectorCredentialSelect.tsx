6296924d0de1fc9cc701ce14a8fc5c26dc1f2fde
Jacob Rothstein <hi@jbr.me>
2023-10-13 17:10:27 -0700

    only allow using a collector credential with a token hash present when the leader is +TokenHash

--- app/src/tasks/TaskForm/CollectorCredentialSelect.tsx
+++ app/src/tasks/TaskForm/CollectorCredentialSelect.tsx
@@ -1,23 +1,37 @@
-import { Await, useLoaderData } from "react-router-dom";
 import FormControl from "react-bootstrap/FormControl";
 import FormSelect from "react-bootstrap/FormSelect";
-import React, { Suspense } from "react";
-import { CollectorCredential } from "../../ApiClient";
+import React from "react";
+import { Aggregator, CollectorCredential } from "../../ApiClient";
 import { Props, TaskFormGroup } from ".";
 import { ShortHelpAndLabel } from "./HelpText";
+import { useLoaderPromise } from "../../util";
 
 export default function CollectorCredentialSelect(props: Props) {
-  const { collectorCredentials } = useLoaderData() as {
-    collectorCredentials: Promise<CollectorCredential[]>;
-  };
-  const { setFieldValue } = props;
+  const collectorCredentials = useLoaderPromise<CollectorCredential[]>(
+    "collectorCredentials",
+    [],
+  );
+  const aggregators = useLoaderPromise<Aggregator[]>("aggregators", []);
+  const leader = React.useMemo(
+    () =>
+      aggregators.find(({ id }) => id === props.values.leader_aggregator_id),
+    [props.values.leader_aggregator_id, aggregators],
+  );
+  const enabledCredentials = React.useMemo(
+    () =>
+      leader && leader.features.includes("TokenHash")
+        ? collectorCredentials.filter(
+            (collectorCredential) => !!collectorCredential.token_hash,
+          )
+        : collectorCredentials,
+    [collectorCredentials, leader],
+  );
 
   React.useEffect(() => {
-    collectorCredentials.then((configs) => {
-      if (configs.length === 1)
-        setFieldValue("collector_credential_id", configs[0].id);
-    });
-  }, [collectorCredentials, setFieldValue]);
+    if (enabledCredentials.length === 1) {
+      props.setFieldValue("collector_credential_id", enabledCredentials[0].id);
+    }
+  }, [enabledCredentials, props.setFieldValue]);
 
   return (
     <TaskFormGroup controlId="collector_credential_id">
@@ -30,21 +44,11 @@ export default function CollectorCredentialSelect(props: Props) {
         id="collector-credential-id"
         name="collector_credential_id"
       >
-        <option disabled></option>
-        <Suspense fallback={<option disabled>...</option>}>
-          <Await resolve={collectorCredentials}>
-            {(collectorCredentials: CollectorCredential[]) =>
-              collectorCredentials.map((collectorCredential) => (
-                <option
-                  key={collectorCredential.id}
-                  value={collectorCredential.id}
-                >
-                  {collectorCredential.name}
-                </option>
-              ))
-            }
-          </Await>
-        </Suspense>
+        {enabledCredentials.map((collectorCredential) => (
+          <option key={collectorCredential.id} value={collectorCredential.id}>
+            {collectorCredential.name}
+          </option>
+        ))}
       </FormSelect>
       <FormControl.Feedback type="invalid">
         {props.errors.collector_credential_id}
