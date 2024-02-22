0cf6bfc3142c905e69a71558ce77d0fa19d7f9ec
Ameer Ghani <inahga@divviup.org>
2024-02-22 12:54:39 -0500

    Fix bug in deserializing unknown features

--- src/entity/aggregator/feature.rs
+++ src/entity/aggregator/feature.rs
@@ -4,6 +4,7 @@ use std::collections::HashSet;
 #[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub enum Feature {
     TokenHash,
+    #[serde(untagged)]
     Unknown(String),
 }
 
@@ -25,5 +26,72 @@ impl From<Feature> for Features {
 impl FromIterator<Feature> for Features {
     fn from_iter<T: IntoIterator<Item = Feature>>(iter: T) -> Self {
         Self(iter.into_iter().collect())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use crate::entity::aggregator::{Feature, Features};
+
+    #[test]
+    fn features_none() {
+        assert_eq!(
+            serde_json::from_value::<Features>(json!([])).unwrap(),
+            Features::from_iter([])
+        )
+    }
+
+    #[test]
+    fn features_token_hash() {
+        assert_eq!(
+            serde_json::from_value::<Features>(json!(["TokenHash"])).unwrap(),
+            Features::from_iter([Feature::TokenHash])
+        );
+        assert_eq!(
+            serde_json::from_value::<Features>(json!(["TokenHash", "TokenHash", "TokenHash"]))
+                .unwrap(),
+            Features::from_iter([Feature::TokenHash])
+        );
+    }
+
+    #[test]
+    fn features_unknown() {
+        assert_eq!(
+            serde_json::from_value::<Features>(json!(["UnspecifiedUnknownFeature"])).unwrap(),
+            Features::from_iter([Feature::Unknown("UnspecifiedUnknownFeature".to_string())])
+        );
+
+        assert_eq!(
+            serde_json::from_value::<Features>(json!([
+                "UnspecifiedUnknownFeature",
+                "UnspecifiedUnknownFeature",
+                "UnspecifiedUnknownFeature"
+            ]))
+            .unwrap(),
+            Features::from_iter([Feature::Unknown("UnspecifiedUnknownFeature".to_string())])
+        );
+
+        assert_eq!(
+            serde_json::from_value::<Features>(json!([
+                "UnspecifiedUnknownFeature",
+                "AnotherUnspecifiedUnknownFeature"
+            ]))
+            .unwrap(),
+            Features::from_iter([
+                Feature::Unknown("UnspecifiedUnknownFeature".to_string()),
+                Feature::Unknown("AnotherUnspecifiedUnknownFeature".to_string())
+            ])
+        );
+
+        assert_eq!(
+            serde_json::from_value::<Features>(json!(["UnspecifiedUnknownFeature", "TokenHash"]))
+                .unwrap(),
+            Features::from_iter([
+                Feature::TokenHash,
+                Feature::Unknown("UnspecifiedUnknownFeature".to_string())
+            ])
+        );
     }
 }
