d072c4b73682f6d2e5bb7b37d10c78771c2a1e4d
David Cook <dcook@divviup.org>
2024-04-24 16:29:50 -0500

    Enable strict mode

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 //import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -24,7 +24,11 @@ function App() {
   );
 }
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
