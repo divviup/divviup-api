ce87990c3fd26c6d3099936118a84c3a5524d12b
Brandon Pitman <bran@bran.land>
2024-05-20 13:59:38 -0700

    Further fixes for new major version.

--- app/src/reportWebVitals.ts
+++ app/src/reportWebVitals.ts
@@ -1,6 +1,6 @@
-import { ReportCallback } from "web-vitals";
+import { MetricType } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportCallback) => {
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
       onCLS(onPerfEntry);
