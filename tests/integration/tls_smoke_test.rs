cecdc9cbdd9c406c1dcf5b351b0c201bc215394d
Tim Geoghegan <timg@divviup.org>
2024-06-24 10:09:16 -0700

    install TLS crypto provider in test

--- tests/integration/tls_smoke_test.rs
+++ tests/integration/tls_smoke_test.rs
@@ -8,6 +8,11 @@ use trillium_tokio::ClientConfig;
 
 #[tokio::test]
 async fn https_connection() {
+    // Choose aws-lc-rs as the default rustls crypto provider. This is what's currently enabled by
+    // the default Cargo feature. Specifying a default provider here prevents runtime errors if
+    // another dependency also enables the ring feature.
+    let _ = trillium_rustls::rustls::crypto::aws_lc_rs::default_provider().install_default();
+
     let self_signed = generate_simple_self_signed(["localhost".into()]).unwrap();
 
     let stopper = Stopper::new();
