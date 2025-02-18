8c90c6e190ed387575a03ddd5143a467cdc2eec9
J.C. Jones <jc@insufficient.coffee>
2025-02-18 14:35:27 -0700

    Adopt the `opentelemetry-rust` 0.28 `Resource` changes [1]
    This moves to a builder pattern for construction of the otel `Resource`. The
    previous version used the Resource::default() constructor which gathered
    attributes from environment variables; in 0.28 that is done via `builder()`
    via it including the EnvResourceDetector [2].
    
    [1] https://github.com/open-telemetry/opentelemetry-rust/blob/main/docs/migration_0.28.md#resource-changes
    [2] https://github.com/open-telemetry/opentelemetry-rust/blob/189078d7a92e534d67137ffaed8e35456eaa589b/opentelemetry-sdk/src/resource/mod.rs#L62

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -16,26 +16,27 @@ pub fn metrics_exporter() -> Result<impl trillium::Handler, MetricError> {
         .build()
         .unwrap();
 
-    // Note that the implementation of `Default` pulls in attributes set via environment variables.
-    let default_resource = Resource::default();
-
     let mut git_revision: &str = git_version!(fallback = "unknown");
     if git_revision == "unknown" {
         if let Some(value) = option_env!("GIT_REVISION") {
             git_revision = value;
         }
     }
-    let version_info_resource = Resource::new([
-        KeyValue::new("service.name", "divviup-api"),
-        KeyValue::new(
+
+    // Note that builder() includes the EnvResourceDetector, which
+    // provides further configuration via environment variables.
+    let resource = Resource::builder()
+        .with_service_name("divviup-api")
+        .with_attribute(KeyValue::new(
             "service.version",
             format!("{}-{}", env!("CARGO_PKG_VERSION"), git_revision),
-        ),
-        KeyValue::new("process.runtime.name", "Rust"),
-        KeyValue::new("process.runtime.version", env!("RUSTC_SEMVER")),
-    ]);
-
-    let resource = default_resource.merge(&version_info_resource);
+        ))
+        .with_attribute(KeyValue::new("process.runtime.name", "Rust"))
+        .with_attribute(KeyValue::new(
+            "process.runtime.version",
+            env!("RUSTC_SEMVER"),
+        ))
+        .build();
 
     global::set_meter_provider(
         SdkMeterProvider::builder()
