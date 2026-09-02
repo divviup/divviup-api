e5bd86fa428e58958b142747ca746967c76cf40b
David Cook <dcook@divviup.org>
2026-09-02 08:31:54 -0500

    Install and configure Vitest

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -1,5 +1,3 @@
-import "@testing-library/jest-dom";
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import Router from "./router.js";
 test("tests have not been written yet but we need at least one test", () => {
