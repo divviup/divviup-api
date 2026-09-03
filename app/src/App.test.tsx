fbb2ac0f32e464340813c2ecc05d7d52ec678d5e
David Cook <dcook@divviup.org>
2026-09-03 09:43:48 -0500

    Render App instead of Router in smoke test

--- app/src/App.test.tsx
+++ app/src/App.test.tsx
@@ -4,8 +4,8 @@ test("smoke test of accounts list via router", async () => {
   // We make our first request for /api_url while importing ApiClientContext, so
   // we need to delay importing this module until after test mocks have been set
   // up.
-  const routerModule = await import("./router.js");
-  const Router = routerModule.default;
-  render(<Router />);
+  const appModule = await import("./App.js");
+  const App = appModule.default;
+  render(<App />);
   expect(await screen.findByText("Test account")).toBeTruthy();
 });
