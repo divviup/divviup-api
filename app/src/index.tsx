496a422cf49353eb70c2176d06ab768e0bd7970b
David Cook <dcook@divviup.org>
2026-09-22 15:54:45 -0500

    Remove web-vitals dependency and dead code

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-//import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
@@ -29,8 +28,3 @@ root.render(
     <App />
   </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-//reportWebVitals(console.log);
